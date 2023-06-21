3b7fdaed8f5516c8287942d54cec61821cdb667e
JoelP-11 <Jp421513@gmail.com>
2023-06-21 15:12:19 -0700

    I think index.js is good now

--- index.js
+++ index.js
@@ -8,3 +8,57 @@ const {
     addEmployee,
     updateEmployeeRole,
 } = require('./db');
+
+function startApp() {
+    inquirer
+        .createPromptModule([
+            {
+                type: 'list',
+                name: 'action',
+                message: 'What would you like to do?',
+                choices: [
+                    'View all departments',
+                    'View all roles',
+                    'View all employees',
+                    'Add a department',
+                    'Add a role',
+                    'Add an employee',
+                    'Update an employee role',
+                    'Exit',
+                ],
+            },
+        ])
+        .then((answers) => {
+            switch (answers.action) {
+                case 'View all departments':
+                    viewAllDepartments();
+                    break;
+                case 'View all roles':
+                    viewAllRoles();
+                    break;
+                case 'View all employees':
+                    viewAllEmployees();
+                    break;
+                case 'Add a department':
+                    addDepartment();
+                    break;
+                case 'Add a role':
+                    addRole();
+                    break;
+                case 'Add an employee':
+                    addEmployee();
+                    break;
+                case 'Update an employee role':
+                    updateEmployeeRole();
+                    break;
+                case 'Exit':
+                    connection.end();
+                    console.log('Goodbye!');
+                    return;
+            }
+            startApp();
+        })
+        .catch((error) => {
+            console.log('An error ocurred:', error);
+        });
+}
